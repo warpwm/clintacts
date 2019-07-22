19e9c52ac2145d2dd22cea8c8f20040be047b7f6
Trim Bresilla <trim.bresilla@gmail.com>
2019-07-22 00:24:58 +0000

    contact class 70%

--- src/inc/contacts.cpp
+++ src/inc/contacts.cpp
@@ -1,13 +1,13 @@
 #include "contacts.h"
 
-string getUserInput(string label, string errorMsg, string exp) {
+string userInput(string label, string errorMsg, string exp) {
     string input;
-    smatch base_match;
-    regex base_regex(exp);
+    smatch match;
+    regex reg(exp);
     while(true) {
         cout << label;
         getline(cin, input);
-        if (regex_match(input, base_match, base_regex )) {
+        if (regex_match(input, match, reg)) {
             return input;
         } else {
             cout << errorMsg;
@@ -16,17 +16,17 @@ string getUserInput(string label, string errorMsg, string exp) {
 }
 
 contact::contact(){
-    cout << "\x1B[2J\x1B[H";
+    // cout << "\x1B[2J\x1B[H";
     cout << "Enter details for the new contact:" << endl;
-    name = getUserInput("Name: ", "Invalid format. You can only use characters.\n", "[a-öA-Ö ]+");
-    address = getUserInput("Address: ", "Invalid format. You can only use characters and digits.\n", "[a-öA-Ö0-9 ]+");
-    email_personal = getUserInput("Email: ", "Invalid format. Email adresses must contain '@' and can only contain characters, periods and underscores.\n", "[._a-ö0-9]+@[a-ö.]+");
-    email_work = getUserInput("Email: ", "Invalid format. Email adresses must contain '@' and can only contain characters, periods and underscores.\n", "[._a-ö0-9]+@[a-ö.]+");
-    mobile = getUserInput("Phone: ", "Invalid format. The format should be: 'xxxyyxxyyy'\n", "[0-9]{10}");
-    birthday = getUserInput("Birthday: ", "Invalid format. The format should be: 'yyyymmdd'\n", "[0-9]{8}");
-    website = getUserInput("Miscellaneous: ", "Invalid format. Can't use newline.\n", ".+");
-    misc = getUserInput("Miscellaneous: ", "Invalid format. Can't use newline.\n", ".+");
-    cout << "Contact has been added!" << endl;
+    name = userInput("Name: ", "Invalid format. You can only use characters.\n", "[a-zA-Z ]+");
+    address = userInput("Address: ", "Invalid format. You can only use characters and digits.\n", ".+");
+    email_personal = userInput("Email: ", "Invalid format. Email adresses must contain '@' and can only contain characters, periods and underscores.\n", "[._a-u0-9]+@[a-z.]+");
+    email_work = userInput("Email: ", "Invalid format. Email adresses must contain '@' and can only contain characters, periods and underscores.\n", "[._a-z0-9]+@[a-z.]+");
+    mobile = userInput("Phone: ", "Invalid format. The format should be: 'xxxyyxxyyy'\n", "[0-9]{7,10}");
+    birthday = userInput("Birthday: ", "Invalid format. The format should be: 'yyyymmdd'\n", "[0-9]{8}");
+    website = userInput("Website: ", "Invalid format. Can't use newline.\n", "https?://[-._a-z0-9]+.[a-z]+");
+    misc = userInput("Miscellaneous: ", "Invalid format. Can't use newline.\n", ".+");
+    cout << "Contact has been created!" << endl;
 }
 
 void contact::printContact() {
--- src/inc/contacts.h
+++ src/inc/contacts.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <regex>
 #include <string>
